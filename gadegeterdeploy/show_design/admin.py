eac0e8fb94066c1fcc62267514112f73d0f7deaa
hiroemon-m <73891312+hiroemon-m@users.noreply.github.com>
2022-12-09 18:54:53 +0900

    Delete gadegeterdeploy directory

--- gadegeterdeploy/show_design/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Image,ImageTag,Tag
-
-# Register your models here.
-admin.site.register(Image)
-admin.site.register(ImageTag)
-admin.site.register(Tag)
-
--- gadegeterdeploy/show_design/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShowDesignConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'show_design'
